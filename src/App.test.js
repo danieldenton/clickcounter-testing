1754b55bbc429f988c71009bc3cabda796d2028f
danieldenton <danielmdenton@gmail.com>
2023-01-06 15:00:53 -0800

    5tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,16 +4,38 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const setup = () => shallow(<App />);
+
+const findByTestAtt = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
 test("renders without error", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAtt(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
 });
 
-test("renders increment button", () => {});
+test("renders increment button", () => {
+  const wrapper = setup();
+  const button = findByTestAtt(wrapper, "increment-button");
+  expect(button.length).toBe(1);
+});
 
-test("renders counter display", () => {});
+test("renders counter display", () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAtt(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
+});
 
-test("counter display starts at 0", () => {});
+test("counter display starts at 0", () => {
+  const wrapper = setup();
+  const count = findByTestAtt(wrapper, "count").text();
+  expect(count).toBe("0");
+});
 
-test("clicking button increments the counter", () => {});
+test("clicking button increments the counter", () => {
+  const wrapper = setup();
+  const button = findByTestAtt(wrapper, "increment-button");
+  button.simulate("click");
+  const count = findByTestAtt(wrapper, "count").text();
+  expect(count).toBe("1");
+});
